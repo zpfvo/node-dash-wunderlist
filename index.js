a48e2989ca664afac7290a25a54f1f86d38db7fa
zpfvo <florian.voit@campus.tu-berlin.de>
2016-10-24 22:54:54 +0200

    [still broken]

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
+'use strict';
 //warning this may trigger multiple times for one press
 //...usually triggers twice based on testing for each press
 var conf = require('./conf.js');
 var dash_button = require('node-dash-button');
 var dash = dash_button("00:bb:3a:a0:e5:ac", null, null, 'all'); //address from step above
-var WunderlistSDK = require('wunderlist');
-var wunderlistAPI = new WunderlistSDK(conf.wunderlist_auth);
-var TasksService = require('./node_modules/wunderlist/public/services/Tasks');
-var tasks = new TasksService();
-// wunderlistAPI.http.lists.all()
-//   .done(function (lists) {
-//     console.log(lists)
-//   })
 
-var create_test =  task.create({
-      'list_id': 271721562,
-      'title': 'Test :)'
-    })
-    .done(function (taskData, statusCode) {
-      // ...
-    })
-    .fail(function (resp, code) {
-      // ...
-    });
+
+var request = require('request')
+
+var create_task = {
+  url: 'https://a.wunderlist.com/api/v1/tasks',
+  method: 'PUT',
+  headers: {
+    'Content-Type':'application/json',
+    'X-Client-ID': conf.wunderlist_auth.clientID,
+    'X-Access-Token': conf.wunderlist_auth.accessToken
+  }
+  ,body: JSON.stringify({'list_id': 271721562, 'title': 'test'})
+};
+
+function callback(error, response, body) {
+  console.log(response, body);
+  if (!error && response.statusCode == 200) {
+    console.log(body, response);
+  }
+}
+
+//request(create_task, callback)
+request(create_task, callback)
+
 
 
 dash.on("detected", function (){
